12faa7aef21cad75b800b68af94ebcbcc19c9f8a
Kamilush <kamil1574@gmail.com>
2021-10-21 17:32:28 +0200

    Lab3.2 Prawie działa, ale clockwise wywołuje exception

---
+++ app/src/main/java/com/example/myapplication/OnButtonClickListener.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+public interface OnButtonClickListener {
+    public void onButtonClickShuffle();
+    public void onButtonClickClockwise();
+    public void onButtonClickHide();
+    public void onButtonClickRestore();
+}
